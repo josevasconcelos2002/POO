1b3e68e28c8f2374b30c7b8b65233352cd9e0120
José Vasconcelos <a100763@alunos.uminho.pt>
2023-04-24 10:59:36 +0100

    Mais métodos para classe das Encomendas

--- Vintage/src/main/java/Encomendas/Encomendas.java
+++ Vintage/src/main/java/Encomendas/Encomendas.java
@@ -24,7 +24,71 @@ public class Encomendas {
     public Encomendas(){
         this.precoFinal = 0.0;
         //this.dataExpedicao =
-        this.estado = estado.PENDENTE;
-        this.dimensao = dimensao.MEDIA;
+        this.estado = Estado.PENDENTE;
+        this.dimensao = Dimensao.MEDIA;
+    }
+
+    public Encomendas(Estado estado, Dimensao dimensao, double precoFinal, LocalDate dataExpedicao){
+        this.estado = estado;
+        this.dimensao = dimensao;
+        this.precoFinal = precoFinal;
+        this.dataExpedicao = dataExpedicao;
+    }
+
+    public Estado getEstado(){
+        return this.estado;
+    }
+
+    public void setEstado(Estado estado){
+        this.estado = estado;
+    }
+
+    public Dimensao getDimensao(){
+        return this.dimensao;
+    }
+
+    public void setDimensao(Dimensao dimensao){
+        this.dimensao = dimensao;
+    }
+
+    public double getPrecoFInal(){
+        return this.precoFinal;
+    }
+
+    public void setPrecoFinal(double precoFinal){
+        this.precoFinal = precoFinal;
+    }
+
+    public LocalDate getDataExpedicao(){
+        return this.dataExpedicao;
+    }
+
+    public void setDataExpedicao(LocalDate dataExpedicao){
+        this.dataExpedicao = dataExpedicao;
+    }
+
+    @Override
+    public String toString(){
+        return "Encomenda{" +
+                "estado='" + this.estado + '\'' +
+                ", dimensao=" + this.dimensao +
+                ", precoFinal=" + this.precoFinal +
+                ", dataExpedicao=" + this.dataExpedicao +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o){
+        boolean resultado = true;
+        if(this == o) resultado = true;
+        if(o == null || this.getClass() != o.getClass()) resultado = false;
+
+        Encomendas encomenda = (Encomendas) o;
+
+        if(this.estado != encomenda.estado) resultado = false;
+        if(this.dimensao != encomenda.dimensao) resultado = false;
+        if(Double.compare(this.precoFinal, encomenda.precoFinal) != 0) resultado = false;
+        if(this.dataExpedicao.equals(encomenda.dataExpedicao) == false) resultado = false;
+        return resultado;
     }
 }
