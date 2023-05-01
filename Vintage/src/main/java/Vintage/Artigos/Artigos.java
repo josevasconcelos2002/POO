a165b39c8e19937cfb301c2fc904fabbe445a6a4
José Vasconcelos <a100763@alunos.uminho.pt>
2023-05-01 22:04:26 +0100

    Lista de artigos implementada

--- Vintage/src/main/java/Vintage/Artigos/Artigos.java
+++ Vintage/src/main/java/Vintage/Artigos/Artigos.java
@@ -6,10 +6,10 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Artigos implements Serializable {
-    private Map<String, Artigo> listaDeArtigos;
+    private Map<Object, Object> listaDeArtigos;
 
     public Artigos(){
-        this.listaDeArtigos = new HashMap<String,Artigo>();
+        this.listaDeArtigos = new HashMap<Object,Object>();
     }
 
     public Artigos(Map<String, Artigo> listaDeArtigos){
@@ -24,12 +24,52 @@ public class Artigos implements Serializable {
         return 0;
     }
 
-    public Map<String, Artigo> getListaDeArtigos() {
+    public Map<Object, Object> getListaDeArtigos() {
         return this.listaDeArtigos.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (a,b) -> a, HashMap::new));
     }
 
     public void setListaDeArtigos(Map<String, Artigo> listaDeArtigos){
         this.listaDeArtigos = listaDeArtigos.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, e->e.getValue().clone(), (a,b)->a, HashMap::new));
+    }
+
+    public void addArtigo(Artigo a){
+        this.listaDeArtigos.put(a.getCodigo(), (Artigo) a.clone());
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Lista de artigos {\n");
+        for (Object artigo : listaDeArtigos.values()) {
+            sb.append("\t").append(artigo.toString()).append("\n");
+        }
+        sb.append("}");
+        return sb.toString();
+    }
+
+    public Artigos clone(){
+        return new Artigos(this);
+    }
+
+    public Artigo getArtigoByCodigo(String codigo){
+        if (this.listaDeArtigos.containsKey(codigo))
+            return (Artigo) this.listaDeArtigos.get(codigo).clone();
+        return null;
+    }
+    /*
+    public Artigo getUserByCode(int code){
+        User conta = this.users.values().stream()
+                .reduce(null, (acc, x) -> (x.getCodigo() == code) ? x : acc);
+        if(conta != null) return conta.clone();
+        else return null;
+    }
+    */
+
+    public boolean existeCodigo(String codigo){
+        return this.listaDeArtigos.containsKey(codigo);
+    }
+
+    public int size(){
+        return listaDeArtigos.size();
     }
 
 }
