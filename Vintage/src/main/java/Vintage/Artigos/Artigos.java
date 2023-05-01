18bf8faed896b071d19c67b2a9f1b72672c402cf
José Vasconcelos <a100763@alunos.uminho.pt>
2023-05-01 18:57:12 +0100

    Inicio map artigos

--- Vintage/src/main/java/Vintage/Artigos/Artigos.java
+++ Vintage/src/main/java/Vintage/Artigos/Artigos.java
@@ -1,7 +1,9 @@
 package Vintage.Artigos;
+
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Artigos implements Serializable {
     private Map<String, Artigo> listaDeArtigos;
@@ -10,8 +12,24 @@ public class Artigos implements Serializable {
         this.listaDeArtigos = new HashMap<String,Artigo>();
     }
 
-    public Artigos(Map<String,Artigo> listaDeArtigos){
-        this.listaDeArtigos = new HashMap<>(listaDeArtigos);
+    public Artigos(Map<String, Artigo> listaDeArtigos){
+        this.setListaDeArtigos(listaDeArtigos);
+    }
+
+    public Artigos(Artigos as){
+        this.listaDeArtigos = as.getListaDeArtigos();
+    }
+
+    public int getNewCode(){
+        return 0;
+    }
+
+    public Map<String, Artigo> getListaDeArtigos() {
+        return this.listaDeArtigos.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (a,b) -> a, HashMap::new));
+    }
+
+    public void setListaDeArtigos(Map<String, Artigo> listaDeArtigos){
+        this.listaDeArtigos = listaDeArtigos.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, e->e.getValue().clone(), (a,b)->a, HashMap::new));
     }
 
 }
